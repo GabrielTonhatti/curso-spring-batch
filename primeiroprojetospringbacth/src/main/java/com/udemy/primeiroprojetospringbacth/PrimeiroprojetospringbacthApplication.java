d073d1cc34b1f7c57f6190cb5cec357a79e1528e
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-07-06 21:45:36 -0300

    First Commit

---
+++ primeiroprojetospringbacth/src/main/java/com/udemy/primeiroprojetospringbacth/PrimeiroprojetospringbacthApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.primeiroprojetospringbacth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroprojetospringbacthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroprojetospringbacthApplication.class, args);
+	}
+
+}
---
+++ primeiroprojetospringbacth/src/test/java/com/udemy/primeiroprojetospringbacth/PrimeiroprojetospringbacthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.primeiroprojetospringbacth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroprojetospringbacthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
