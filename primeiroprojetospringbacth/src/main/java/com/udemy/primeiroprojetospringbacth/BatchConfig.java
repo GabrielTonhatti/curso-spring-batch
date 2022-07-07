75fe0ff540107ae5ec72e68cd08b6855b168a842
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-07-06 22:04:14 -0300

    Prática: Criação de um step chunk-based

--- primeiroprojetospringbacth/src/main/java/com/udemy/primeiroprojetospringbacth/BatchConfig.java
+++ primeiroprojetospringbacth/src/main/java/com/udemy/primeiroprojetospringbacth/BatchConfig.java
@@ -8,11 +8,17 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.function.FunctionItemProcessor;
+import org.springframework.batch.item.support.IteratorItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableBatchProcessing
@@ -49,5 +55,40 @@ public class BatchConfig {
         };
     }
 
+    @Bean
+    public Job imprimeParImparJob() {
+        return jobBuilderFactory
+                .get("imprimeParImparJob")
+                .start(imprimeParImparStep())
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+
+    public Step imprimeParImparStep() {
+        return stepBuilderFactory
+                .get("imprimeParImparStep")
+                .<Integer, String>chunk(1)
+                .reader(contaAteDezReader())
+                .processor(parOuImparProcessor())
+                .writer(imprimeWriter())
+                .build();
+    }
+
+    private IteratorItemReader<Integer> contaAteDezReader() {
+        List<Integer> numerosDeUmAteDez = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        return new IteratorItemReader<>(numerosDeUmAteDez.iterator());
+    }
+
+    private FunctionItemProcessor<Integer, String> parOuImparProcessor() {
+        return new FunctionItemProcessor<>(
+                (item -> item % 2 == 0 ? String.format("Item %s é Par", item)
+                        : String.format("Item %s é Impar", item))
+        );
+    }
+
+    private ItemWriter<? super String> imprimeWriter() {
+        return itens -> itens.forEach(System.out::println);
+    }
 
 }
