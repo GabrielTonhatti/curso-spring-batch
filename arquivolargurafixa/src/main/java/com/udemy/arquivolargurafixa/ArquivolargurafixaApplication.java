2b6cc6727eefc480efb16f2d01a02ad248e8d67e
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-07-14 19:53:32 -0300

    Leitura de arquivo com largura fixa com Spring Batch

---
+++ arquivolargurafixa/src/main/java/com/udemy/arquivolargurafixa/ArquivolargurafixaApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.arquivolargurafixa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquivolargurafixaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquivolargurafixaApplication.class, args);
+	}
+
+}
---
+++ arquivolargurafixa/src/main/java/com/udemy/arquivolargurafixa/writer/LeituraArquivoLarguraFixaWriterConfig.java
@@ -0,0 +1,14 @@
+package com.udemy.arquivolargurafixa.writer;
+
+import com.udemy.arquivolargurafixa.dominio.Cliente;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LeituraArquivoLarguraFixaWriterConfig {
+    @Bean
+    public ItemWriter<Cliente> leituraArquivoLarguraFixaWriter() {
+        return items -> items.forEach(System.out::println);
+    }
+}
