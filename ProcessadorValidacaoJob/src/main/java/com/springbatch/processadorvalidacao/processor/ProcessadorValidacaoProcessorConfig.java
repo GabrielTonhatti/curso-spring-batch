7b56723a3fd0aedf82e8b00cfe95a8921dc8dbde
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-07-26 19:48:20 -0300

    Prática: Processadores de validação

---
+++ ProcessadorValidacaoJob/src/main/java/com/springbatch/processadorvalidacao/processor/ProcessadorValidacaoProcessorConfig.java
@@ -0,0 +1,39 @@
+package com.springbatch.processadorvalidacao.processor;
+
+import com.springbatch.processadorvalidacao.dominio.Cliente;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.validator.ValidatingItemProcessor;
+import org.springframework.batch.item.validator.ValidationException;
+import org.springframework.batch.item.validator.Validator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Configuration
+public class ProcessadorValidacaoProcessorConfig {
+
+    private Set<String> emails = new HashSet<>();
+
+    @Bean
+    public ItemProcessor<Cliente, Cliente> procesadorValidacaoProcessor() {
+//		BeanValidatingItemProcessor<Cliente> processor = new BeanValidatingItemProcessor<>();
+//		processor.setFilter(true);
+        ValidatingItemProcessor<Cliente> processor = new ValidatingItemProcessor<>();
+        processor.setValidator(validator());
+        processor.setFilter(true);
+        
+        return processor;
+    }
+
+    private Validator<Cliente> validator() {
+        return cliente -> {
+            if (emails.contains(cliente.getEmail())) {
+                throw new ValidationException(String.format("O cliente %s já foi processado!", cliente.getEmail()));
+            }
+
+            emails.add(cliente.getEmail());
+        };
+    }
+}
